08c1ae6aa473d9dc1d20cb49de544b9575851207
Arturoqq <joserojasr23@gmail.com>
2024-06-08 22:25:39 -0500

    basic home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:backoapp/screens/menu_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,43 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, "searchScreen");
+                    },
+                    child: Container(
+                          height: size.height * 0.04,
+                          width: size.width * 0.35,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            border: Border.all(
+                              color: secondaryColor,
+                              width: 2.0,
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              options[0],
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 14,
+                                  color: secondaryColor),
+                            ),
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+      ],
+    );
   }
 }
